01a55df081d88fab30b9694990d3ef5c750e3a4c
josuemora <josue.mora@omnilife.com>
2014-05-21 12:27:26 -0500

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by josue mora on 21/05/14.
+//  Copyright (c) 2014 josue mora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by josue mora on 21/05/14.
+//  Copyright (c) 2014 josue mora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
